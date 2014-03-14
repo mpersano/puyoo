49a7941f173523358c5fe2fb0e78a19952d344a4
fzort <fzort@tycho.luna>
2014-03-14 11:38:25 +0000

    Forgot this...

---
+++ new.cc
@@ -0,0 +1,30 @@
+#include <types.h>
+
+extern "C" {
+void *malloc(size_t);
+void free(void *);
+};
+
+void *
+operator new(size_t size)
+{
+	return malloc(size);
+}
+
+void *
+operator new[](size_t size)
+{
+	return malloc(size);
+}
+
+void
+operator delete(void *ptr)
+{
+	free(ptr);
+}
+
+void
+operator delete[](void *ptr)
+{
+	free(ptr);
+}
