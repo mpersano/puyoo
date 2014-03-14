9ec6296f0d04894862685740d07f935512c2774b
fzort <fzort@tycho.luna>
2014-03-14 16:56:45 +0000

    duh

--- gpu.h
+++ gpu.h
@@ -5,11 +5,11 @@
 
 namespace psx { namespace gpu {
 
-static const uint32_t GPU_CONTROL_PORT		= 0x1f801814;
-static const uint32_t GPU_DATA_PORT		= 0x1f801810;
-static const uint32_t DMA_BASE_ADDRESS_PORT	= 0x1f8010a0;
-static const uint32_t DMA_BLOCK_CONTROL_PORT	= 0x1f8010a4;
-static const uint32_t DMA_CHANNEL_CONTROL_PORT	= 0x1f8010a8;
+static uint32_t *GPU_CONTROL_PORT		= reinterpret_cast<uint32_t *>(0x1f801814);
+static uint32_t *GPU_DATA_PORT			= reinterpret_cast<uint32_t *>(0x1f801810);
+static uint32_t *DMA_BASE_ADDRESS_PORT		= reinterpret_cast<uint32_t *>(0x1f8010a0);
+static uint32_t *DMA_BLOCK_CONTROL_PORT		= reinterpret_cast<uint32_t *>(0x1f8010a4);
+static uint32_t *DMA_CHANNEL_CONTROL_PORT	= reinterpret_cast<uint32_t *>(0x1f8010a8);
 
 enum dma_mode {
 	DMA_DISABLED = 0,
@@ -20,13 +20,13 @@ enum dma_mode {
 inline uint32_t
 gpu_control_port()
 {
-	return *reinterpret_cast<uint32_t *>(GPU_CONTROL_PORT);
+	return *GPU_CONTROL_PORT;
 }
 
 inline void
 gpu_control_port(uint32_t data)
 {
-	*reinterpret_cast<uint32_t *>(GPU_CONTROL_PORT) = data;
+	*GPU_CONTROL_PORT = data;
 }
 
 inline void
@@ -50,31 +50,31 @@ gpu_control_set_dma_mode(dma_mode mode)
 inline uint32_t
 gpu_data_port()
 {
-	return *reinterpret_cast<uint32_t *>(GPU_DATA_PORT);
+	return *GPU_DATA_PORT;
 }
 
 inline void
 gpu_data_port(uint32_t data)
 {
-	*reinterpret_cast<uint32_t *>(GPU_DATA_PORT) = data;
+	*GPU_DATA_PORT = data;
 }
 
 inline void
 gpu_dma_set_base_address(uint32_t address)
 {
-	*reinterpret_cast<uint32_t *>(DMA_BASE_ADDRESS_PORT) = address;
+	*DMA_BASE_ADDRESS_PORT = address;
 }
 
 inline void
 gpu_dma_set_block_control(uint32_t value)
 {
-	*reinterpret_cast<uint32_t *>(DMA_BLOCK_CONTROL_PORT) = value;
+	*DMA_BLOCK_CONTROL_PORT = value;
 }
 
 inline void
 gpu_dma_set_channel_control(uint32_t value)
 {
-	*reinterpret_cast<uint32_t *>(DMA_CHANNEL_CONTROL_PORT) = value;
+	*DMA_CHANNEL_CONTROL_PORT = value;
 }
 
 } };
