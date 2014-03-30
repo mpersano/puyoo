edeecabdbeac285810fee7712c79fb61b4f15cd6
fzort <fzort@tycho.luna>
2014-03-29 21:18:17 -0300

    foo

--- src/in_game.cc
+++ src/in_game.cc
@@ -3,12 +3,15 @@
 #include "in_game.h"
 
 static const int grid_height = GRID_ROWS*BLOCK_SIZE;
-static const int grid_width = GRID_COLS*BLOCK_SIZE;
 static const int base_y = (SCREEN_HEIGHT - grid_height)/2;
 
+static const int grid_width = GRID_COLS*BLOCK_SIZE;
+static const int left_base_x = SCREEN_WIDTH/4 - grid_width/2;
+static const int right_base_x = SCREEN_WIDTH*3/4 - grid_width/2;
+
 in_game::in_game()
-: left_grid_(SCREEN_WIDTH/4 - grid_width/2, base_y, &right_grid_, true)
-, right_grid_(SCREEN_WIDTH*3/4 - grid_width/2, base_y, &left_grid_, false)
+: left_grid_(left_base_x, base_y, &right_grid_, true)
+, right_grid_(right_base_x, base_y, &left_grid_, false)
 { }
 
 void
