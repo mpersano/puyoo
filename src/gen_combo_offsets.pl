35747ce8221ad47896912782c2dff8cec2e24bf4
fzort <fzort@tycho.luna>
2014-04-01 09:34:57 -0300

    cleaner tweening function for combo animation offsets

--- src/gen_combo_offsets.pl
+++ src/gen_combo_offsets.pl
@@ -6,16 +6,9 @@ my ($min_y, $max_y) = (250, -50);
 print "static const int offset[ANIMATION_TICS] = { ";
 
 for my $tic (0 .. $animation_tics - 1) {
+	print "\n" if $tic%8 == 0;
 	my $t = $tic/$animation_tics;
-
-	my $f;
-
-	if ($t < .5) {
-		$f = .5 - .5*(1. - 2.*$t)*(1. - 2.*$t);
-	} else {
-		$f = .5 + .5*(2.*($t - .5))*(2.*($t - .5));
-	}
-
+	my $f = .5 + .5*(2.*$t - 1.)**3;
 	my $y = int($min_y + $f*($max_y - $min_y));
 
 	print "$y, ";
